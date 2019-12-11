d6b017a3e1d77b37d6ed1bad9040a7a31e5c54a5
Matthew Bowen <46889006+S00189001@users.noreply.github.com>
2019-12-11 11:22:29 +0000

    End of Exam - Unable to Complete

--- Exam-S00189001/Exam-S00189001/MainWindow.xaml.cs
+++ Exam-S00189001/Exam-S00189001/MainWindow.xaml.cs
@@ -13,23 +13,86 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+/*
+ * Couldn't get the player generation working
+ */
+
 namespace Exam_S00189001
 {
-    public enum Position {
-        Goalkeeper, //0
-        Defender,   //1
-        Midfielder, //2
-        Forward     //3
-    }
+   
 
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<Player> FullTeamList = new List<Player>();
+        // creates a reference to the player class
+        public Player player;
+        public List<Player> tempTeamList = new List<Player>();
+        public List<Player> selectedPlayersList = new List<Player>();
+
+
         public MainWindow()
         {
             InitializeComponent();
+            //FullTeamList.Add(player.PlayerGeneration(player));
+
+            //creates an instance of player and stores as player1
+            Player player1 = new Player("Mattie", "Bowen", Position.Forward, new DateTime(2000, 5, 10), 24);
+
+            tempTeamList.Add(player1);
+
+        }
+        
+        ////public void TeamCreation()
+        ////{
+        ////    // Create a list of 18
+            
+
+        ////    for (int i = 0; i <= 18; i++)
+        ////    {
+        ////        //FullTeamList.Add(PlayerGeneration(player));
+        ////        player.PlayerList.Add(player.PlayerGeneration(player));
+        ////        FullTeamList = player.PlayerList;
+
+        ////        //Player player = new Player(player.PlayerGeneration.FindName as string, );
+        ////        //player.PlayerGeneration();
+        ////    }
+        ////}
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //TeamCreation();
+            //FullTeamList = player.PlayerList;
+            lstb_AllPlayers.ItemsSource = tempTeamList;
+            lstb_AllPlayers.Items.Refresh();
+        }
+
+        private void Btn_Add_Click(object sender, RoutedEventArgs e)
+        {
+            // creates a variable we can use to store the selected item
+            Player selectedItem;
+            // store the selected item in selected item as player
+            selectedItem = lstb_AllPlayers.SelectedItem as Player;
+            // add the selected item to the selected player list
+            lstb_SelectedPlayers.ItemsSource = selectedItem.PlayerList;
+            // remove the selected item from the All players list
+
+            //updates spaces
+            txtb_Spaces.Text = selectedPlayersList.Count.ToString();
+        }
+
+        private void Btn_Remove_Click(object sender, RoutedEventArgs e)
+        {
+            // creates a variable we can use to store the selected item
+            Player selectedItem;
+            // stores the selected item in the selectedItem variable from selected players
+            selectedItem = lstb_SelectedPlayers.SelectedItem as Player;
+            lstb_AllPlayers.ItemsSource = selectedItem.PlayerList;
+
+            //update spaces
+            txtb_Spaces.Text = selectedPlayersList.Count.ToString();
         }
     }
 }
--- Exam-S00189001/Exam-S00189001/Player.cs
+++ Exam-S00189001/Exam-S00189001/Player.cs
@@ -6,13 +6,26 @@ using System.Threading.Tasks;
 
 namespace Exam_S00189001
 {
+    // Creates an enum that will be used to define by type
+    public enum Position
+    {
+        Goalkeeper, //0
+        Defender,   //1
+        Midfielder, //2
+        Forward     //3
+    }
     public class Player
     {
+        // Declared variable
         public string firstName { get; set; }
         public string surname { get; set; }
         public Position preferredPosition { get; set; }
         public DateTime dob { get; set; }
         public int age { get; set; }
+        public List<Player> PlayerList;
+
+        // Random variable created to use random ranges
+        public Random random = new Random();
 
         public Player (string firstName, string surname, Position preferedPosition, DateTime dob, int age)
         {
@@ -20,7 +33,49 @@ namespace Exam_S00189001
             this.surname = surname;
             this.preferredPosition = preferredPosition;
             this.dob = dob;
-            this.age = age;
+            this.age = (int)DateTime.Now.Subtract(dob).Days / 365; //age;
         }
+
+        public void PlayerGeneration(Player player)
+        {           
+            // array of firstName to be used in the creation of a player
+            string[] firstNames = {
+                "Adam", "Amelia", "Ava", "Chloe", "Conor", "Daniel", "Emily",
+                "Emma", "Grace", "Hannah", "Harry", "Jack", "James",
+                "Lucy", "Luke", "Mia", "Michael", "Noah", "Sean", "Sophie"};
+
+            // array of SecondName to be used in the creation of a player
+            string[] lastNames = {
+                "Brennan", "Byrne", "Daly", "Doyle", "Dunne", "Fitzgerald", "Kavanagh",
+                "Kelly", "Lynch", "McCarthy", "McDonagh", "Murphy", "Nolan", "O'Brien",
+                "O'Connor", "O'Neill", "O'Reilly", "O'Sullivan", "Ryan", "Walsh"
+            };
+
+            firstName = firstNames[random.Next(0, 20)];
+            surname = lastNames[random.Next(0, 20)];
+            age = (int)DateTime.Now.Subtract(dob).Days / 365;
+
+            Player player1 = new Player(firstName, surname, Position.Goalkeeper, dob, age);
+            //player = player1;
+            //return player;
+            PlayerList.Add(player1);
+        }
+
+        //public void PlayerCreation()
+        //{
+        //    string[] firstNames = {
+        //        "Adam", "Amelia", "Ava", "Chloe", "Conor", "Daniel", "Emily",
+        //        "Emma", "Grace", "Hannah", "Harry", "Jack", "James",
+        //        "Lucy", "Luke", "Mia", "Michael", "Noah", "Sean", "Sophie"};
+
+        //    string[] lastNames = {
+        //        "Brennan", "Byrne", "Daly", "Doyle", "Dunne", "Fitzgerald", "Kavanagh",
+        //        "Kelly", "Lynch", "McCarthy", "McDonagh", "Murphy", "Nolan", "O'Brien",
+        //        "O'Connor", "O'Neill", "O'Reilly", "O'Sullivan", "Ryan", "Walsh"
+        //    };
+
+        //    firstName = firstNames[random.Next(0, 20)];
+        //    surname = lastNames[random.Next(0, 20)];
+        //}
     }
 }
